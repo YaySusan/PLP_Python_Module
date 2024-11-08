a2416bf89d904bc297a0b1b252725dee5902f9a7
Susan Jonas <45673836+YaySusan@users.noreply.github.com>
2024-11-08 21:50:55 +0200

    Delete intro_to_python_assign_one.py

--- intro_to_python_assign_one.py
+++
@@ -1,9 +0,0 @@
-#Introduction to Python - Assignment 1
-
-# Requesting user's info
-name = input("Please enter your name: ")
-age = input("Please enter your age: ")
-location = input("Please enter your location: ")
-
-# Personalized message
-print(f"Hello there, {name}. I see you are {age} years old and live in {location}. It's nice to meet you.")
